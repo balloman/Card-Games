6a57bca5b8bce531a59a81f2ce826dd5317b5b0d
Bernard Allotey <benard.allotey4@gmail.com>
2018-06-08 16:25:59 -0500

    Initial VS Commit

---
+++ Card-Games/Start.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card_Games
+{
+    class Start
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
